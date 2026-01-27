10f4facc5b71e16c82065e573c627842ebdfde2e
tukaram40k <tukaram40k@gmail.com>
2026-01-27 17:40:47 +0200

    implement add operation test

--- internal/test/internal_test.go
+++ internal/test/internal_test.go
@@ -48,6 +48,49 @@ func TestError(t *testing.T) {
 	}
 }
 
+func TestAdd(t *testing.T) {
+	f1 := []string{"add"}
+	p1 := []string{"arg1"}
+	p2 := []string{"arg1", "arg2", "arg3"}
+	p3 := []string{}
+
+	var got string
+	var execErr error
+
+	got = captureStdout(func() {
+		execErr = internal.Execute(f1, p1)
+	})
+
+	if execErr != nil {
+		t.Errorf("-add with 1 param returned unexpected error: %s", execErr)
+	}
+
+	if got != "saved!\n" {
+		t.Errorf("-add missing save confirmation")
+	}
+
+	got = captureStdout(func() {
+		execErr = internal.Execute(f1, p2)
+	})
+
+	if execErr != nil {
+		t.Errorf("-add with 2 params returned unexpected error: %s", execErr)
+	}
+
+	if got != "saved!\n" {
+		t.Errorf("-add missing save confirmation")
+	}
+
+	err := internal.Execute(f1, p3)
+	if err != nil {
+		if err.Error() != "nothing to save!" {
+			t.Errorf("-add is supposed to warn on empty args")
+		}
+	} else {
+		t.Errorf("-add is supposed to return error on empty params")
+	}
+}
+
 func captureStdout(f func()) string {
 	old := os.Stdout
 	r, w, _ := os.Pipe()
